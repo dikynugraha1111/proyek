27b9d327133dc26fc98ce9ae380e4e618a1e503a
dikynugraha1111 <48794681+dikynugraha1111@users.noreply.github.com>
2020-10-01 20:43:57 +0700

    coba1

--- app/Views/halaman/dataDetail.php
+++ app/Views/halaman/dataDetail.php
@@ -7,7 +7,7 @@
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title"><?= $siswa['nama']; ?></h5>
-                <h6 class="card-subtitle mb-2 text-muted">LOL PEOPLE</h6>
+                <h6 class="card-subtitle mb-2 text-muted">LOL PEOPLEs</h6>
                 <p class="card-text">NISN =<?= $siswa['nisn']; ?>
                     <br>
                     nik =<?= $siswa['nik']; ?> <br>
